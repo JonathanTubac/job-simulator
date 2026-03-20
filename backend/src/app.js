633431ccc362ff81fd14647bb61dc04dd472c402
JonathanTubac <javiertubac1290.e@gmail.com>
2026-03-19 19:51:01 -0600

    feat(backend): Implemented routes at /api in the express server

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import productRouter from './routes/product.routes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', productRouter);
+
+export default app;
